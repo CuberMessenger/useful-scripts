6313c4283c2e7930d6cde6757926d5b3b5d2d319
CuberMessenegr <cuber_messenger@hotmail.com>
2025-11-13 00:03:41 +0800

    fix correct level

--- qr-code-color-map.py
+++ qr-code-color-map.py
@@ -1,14 +1,11 @@
 import os
 import qrcode
-import numpy as np
-
-from PIL import Image
 
 def main():
-    message = ""
+    message = "Elohim lies about the tower."
     qr = qrcode.QRCode(
         version=None,
-        error_correction=qrcode.constants.ERROR_CORRECT_M,
+        error_correction=qrcode.constants.ERROR_CORRECT_H,
         border=0
     )
 
@@ -16,7 +13,7 @@ def main():
 
     qr.make(fit=True)
 
-    image = qr.make_image(fill_color=(219, 105, 44), back_color=(248, 234, 225))
+    image = qr.make_image(fill_color=(0, 0, 0), back_color=(255, 255, 255))
     image.save("qr.png")
 
 if __name__ == "__main__":
