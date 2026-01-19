76f64c01451836afd25e65d73532ee515aeddad7
CuberMessenger <cuber_messenger@hotmail.com>
2026-01-19 11:35:55 +0800

    qr add border

--- qr-code-color-map.py
+++ qr-code-color-map.py
@@ -2,18 +2,19 @@ import os
 import qrcode
 
 def main():
-    message = "Elohim lies about the tower."
+    message = "Nha Nheo Nhen is throwing the ball to the back of the toilet."
     qr = qrcode.QRCode(
         version=None,
         error_correction=qrcode.constants.ERROR_CORRECT_H,
-        border=0
+        box_size=10,
+        border=2,
     )
 
     qr.add_data(message)
 
     qr.make(fit=True)
 
-    image = qr.make_image(fill_color=(0, 0, 0), back_color=(255, 255, 255))
+    image = qr.make_image(fill_color=(255, 195, 62), back_color=(219, 101, 56))
     image.save("qr.png")
 
 if __name__ == "__main__":
